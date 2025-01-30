5e6de1ac9db12961df8b864bf25e924c6c0cc801
vinay961 <vinay100rai@gmail.com>
2025-01-30 17:02:35 +0530

    Inheritance

---
+++ Inheritance/Inheritance/Child.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Inheritance
+{
+    class Child : Parent
+    {
+        public override void method1()
+        {
+            Console.WriteLine("Child method1");
+        }
+        public void method3()
+        {
+            Console.WriteLine("Child method3");
+        }
+        static void Main(string[] args)
+        {
+            Parent p = new Child();
+            p.method1();
+            p.method2();
+            //p.method3();
+        }
+    }
+}
+// If the parent class constructor is private, the child class will not be able to access it and hence inheritance is not possible.
+// In inheritance, the child class inherits the properties and behavior of the parent class, but parent class can not inherit the properties and behavior of the child class.
+// We can initialize a parent class variable using a child class object, but the reverse is not possible. ( Parent p = new Child(); )
+// In the above code, the output for p.method3() will be an error because the parent class does not have a method3() method.
+// For accessing the method3() method, we need to create an object of the child class. ( Child c = new Child(); c.method3(); )
---
+++ Inheritance/Inheritance/Parent.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Inheritance
+{
+    class Parent
+    {
+        public Parent()
+        {
+            Console.WriteLine("Parent constructor");
+        }
+        public virtual void method1()
+        {
+            Console.WriteLine("Parent method1");
+        }
+        public void method2()
+        {
+            Console.WriteLine("Parent method2");
+        }
+    }
+}
